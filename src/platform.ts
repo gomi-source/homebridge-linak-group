309b9ef9385691dac1a15eda8c923867160d425f
Anders Ljungh <anders@gomi.se>
2025-10-28 09:40:21 +0100

    Added MQTT functionality

--- src/platform.ts
+++ src/platform.ts
@@ -1,17 +1,19 @@
 import type { API, Characteristic, DynamicPlatformPlugin, Logging, PlatformAccessory, PlatformConfig, Service } from 'homebridge';
 
-import { ExamplePlatformAccessory } from './platformAccessory.js';
+import { DeskGroupAccessory } from './platformAccessory.js';
 import { PLATFORM_NAME, PLUGIN_NAME } from './settings.js';
 
+import bent from 'bent';
+
 // This is only required when using Custom Services and Characteristics not support by HomeKit
-import { EveHomeKitTypes } from 'homebridge-lib/EveHomeKitTypes';
+// import { EveHomeKitTypes } from 'homebridge-lib/EveHomeKitTypes';
 
 /**
  * HomebridgePlatform
  * This class is the main constructor for your plugin, this is where you should
  * parse the user config and discover/register accessories with Homebridge.
  */
-export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
+export class DeskGroupPlatform implements DynamicPlatformPlugin {
   public readonly Service: typeof Service;
   public readonly Characteristic: typeof Characteristic;
 
@@ -20,10 +22,10 @@ export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
   public readonly discoveredCacheUUIDs: string[] = [];
 
   // This is only required when using Custom Services and Characteristics not support by HomeKit
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  public readonly CustomServices: any;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  public readonly CustomCharacteristics: any;
+   
+  // public readonly CustomServices: any;
+   
+  // public readonly CustomCharacteristics: any;
 
   constructor(
     public readonly log: Logging,
@@ -34,8 +36,28 @@ export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
     this.Characteristic = api.hap.Characteristic;
 
     // This is only required when using Custom Services and Characteristics not support by HomeKit
-    this.CustomServices = new EveHomeKitTypes(this.api).Services;
-    this.CustomCharacteristics = new EveHomeKitTypes(this.api).Characteristics;
+    // this.CustomServices = new EveHomeKitTypes(this.api).Services;
+    // this.CustomCharacteristics = new EveHomeKitTypes(this.api).Characteristics;
+
+    // MY CODE START
+
+    // No configuration found - we won't start.
+    if(!config) {
+      return;
+    }
+
+    // If we don't have any HTTP server configured, we're done here.
+    if(!config.sLinakServerBasePath) {
+      this.log.error('No desk group server path has been configured.');
+      return;
+    }
+
+    if (config.maxHeight < config.baseHeight) {
+      this.log.error('The max height must be higher than the base height.');
+      return;
+    }
+
+    // MY CODE STOP
 
     this.log.debug('Finished initializing platform:', this.config.name);
 
@@ -66,8 +88,15 @@ export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
    * Accessories must only be registered once, previously created accessories
    * must not be registered again to prevent "duplicate UUID" errors.
    */
-  discoverDevices() {
-    // EXAMPLE ONLY
+  async discoverDevices() {
+    // Query REST API for devices
+    let deskGroups = await this.GetGroups();
+    if (this.config.addIndividualDesks) {
+      const desks = await this.GetDesks();
+      deskGroups = [...deskGroups, ...desks];
+    }
+    
+    /* EXAMPLE ONLY
     // A real plugin you would discover accessories from the local network, cloud services
     // or a user-defined array in the platform config.
     const exampleDevices = [
@@ -86,13 +115,16 @@ export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
         CustomService: 'AirPressureSensor',
       },
     ];
-
+    */
     // loop over the discovered devices and register each one if it has not already been registered
-    for (const device of exampleDevices) {
+    console.debug('************ DISCOVERED GROUPS ************');
+    console.debug(deskGroups);
+    // console.log(myVal);
+    for (const device of deskGroups) {
       // generate a unique id for the accessory this should be generated from
       // something globally unique, but constant, for example, the device serial
       // number or MAC address
-      const uuid = this.api.hap.uuid.generate(device.exampleUniqueId);
+      const uuid = this.api.hap.uuid.generate(device.id);
 
       // see if an accessory with the same uuid has already been registered and restored from
       // the cached devices we stored in the `configureAccessory` method above
@@ -108,7 +140,7 @@ export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
 
         // create the accessory handler for the restored accessory
         // this is imported from `platformAccessory.ts`
-        new ExamplePlatformAccessory(this, existingAccessory);
+        new DeskGroupAccessory(this, existingAccessory);
 
         // it is possible to remove platform accessories at any time using `api.unregisterPlatformAccessories`, e.g.:
         // remove platform accessories when no longer present
@@ -116,10 +148,10 @@ export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
         // this.log.info('Removing existing accessory from cache:', existingAccessory.displayName);
       } else {
         // the accessory does not yet exist, so we need to create it
-        this.log.info('Adding new accessory:', device.exampleDisplayName);
+        this.log.info('Adding new accessory:', device.name);
 
         // create a new accessory
-        const accessory = new this.api.platformAccessory(device.exampleDisplayName, uuid);
+        const accessory = new this.api.platformAccessory(device.name, uuid);
 
         // store a copy of the device object in the `accessory.context`
         // the `context` property can be used to store any data about the accessory you may need
@@ -127,7 +159,7 @@ export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
 
         // create the accessory handler for the newly create accessory
         // this is imported from `platformAccessory.ts`
-        new ExamplePlatformAccessory(this, accessory);
+        new DeskGroupAccessory(this, accessory);
 
         // link the accessory to your platform
         this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
@@ -147,4 +179,93 @@ export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
       }
     }
   }
+
+  async GetGroups(): Promise<DeskGroup[]> {
+    const deskGroups: DeskGroup[] = [];
+
+    const basic_auth_string = Buffer.from(this.config.username + ':' + this.config.password).toString('base64');
+    const headers = {
+      'Authorization': 'Basic ' + basic_auth_string,
+    };
+
+    const getJSON = bent('json');
+    const groupData = await getJSON(this.config.sLinakServerBasePath + '/groups', '', headers);
+
+    for (const group_id in groupData) {
+      const group_members: string[] = groupData[group_id];
+      console.debug(group_id);  // "office"
+      console.debug(group_members);  // array of desk_id
+
+      const deskGroup = new DeskGroup(group_id, group_id, groupData[group_id]);
+
+      // Query for info on desks in group
+      for (const desk_id of group_members) {
+        console.debug(desk_id);
+        const deskData = await getJSON(this.config.sLinakServerBasePath + '/desks/' + desk_id, '', headers);
+        console.debug(deskData);
+        deskGroup.desks.push(deskData);
+      }
+      
+      deskGroups.push(deskGroup);
+    }
+
+    return deskGroups;
+  }
+
+  async GetDesks(): Promise<Desk[]> {
+    const desks: Desk[] = [];
+
+    const basic_auth_string = Buffer.from(this.config.username + ':' + this.config.password).toString('base64');
+    const headers = {
+      'Authorization': 'Basic ' + basic_auth_string,
+    };
+
+    const getJSON = bent('json');
+    const response = await getJSON(this.config.sLinakServerBasePath + '/desks', '', headers);
+    // forEach (const desk_id in desks.entries) {
+    for (let i = 0; i < response.length; i++) {
+      const desk = new Desk(response[i].id, response[i].config_name, response[i].height, response[i].base_height);
+      desks.push(desk);
+    }
+
+    return desks;
+  }
+}
+
+export class DeskGroup {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  public desks: any = [];
+
+  constructor(
+    public readonly id: string,
+    public readonly name: string,
+    public readonly mac_ids: string[],
+    public readonly desk_type: DeskGroupType = DeskGroupType.Group,
+  ) {
+    this.id = id;
+    this.name = name;
+    this.mac_ids = mac_ids;
+    this.desk_type = desk_type;
+  }
+}
+
+export class Desk extends DeskGroup {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  public desk: any;
+
+  constructor(
+    public readonly id: string,
+    public readonly config_name: string,
+    public readonly height: number,
+    public readonly base_height: number,
+  ) {
+    super(id, config_name, [], DeskGroupType.Individual);
+    this.height = height;
+    this.base_height = base_height;
+  }
+}
+
+export enum DeskGroupType {
+  Group,
+  Individual
 }
--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -1,28 +1,75 @@
 import type { CharacteristicValue, PlatformAccessory, Service } from 'homebridge';
 
-import type { ExampleHomebridgePlatform } from './platform.js';
+import { DeskGroupType } from './platform.js';
+import type { DeskGroupPlatform } from './platform.js';
+
+import bent from 'bent';
+import { connect, MqttClient } from 'mqtt';
+
 
 /**
  * Platform Accessory
  * An instance of this class is created for each accessory your platform registers
  * Each accessory may expose multiple services of different service types.
  */
-export class ExamplePlatformAccessory {
+export class DeskGroupAccessory {
   private service: Service;
+  private mqtt: MqttClient;
 
-  /**
-   * These are just used to create a working example
-   * You should implement your own code to track the state of your accessory
-   */
-  private exampleStates = {
-    On: false,
-    Brightness: 100,
+  private deskStates = {
+    CurrentPosition: 0,
+    TargetPosition: 0,
   };
 
+  private readonly topics: {
+    readonly height: string;
+  };
+
+  private requestedPosTimer: string | number | NodeJS.Timeout | undefined;
+
   constructor(
-    private readonly platform: ExampleHomebridgePlatform,
+    private readonly platform: DeskGroupPlatform,
     private readonly accessory: PlatformAccessory,
   ) {
+    // MQTT
+    this.topics = {
+      height: `${this.platform.config.mqtt.topicBase}/${accessory.context.device.name}/height`,
+    };
+
+    if (this.platform.config.mqtt.username) {
+      const mqtt_connection_options = {
+        username: this.platform.config.mqtt.username,
+        password: this.platform.config.mqtt.password,
+      };
+      this.mqtt = connect(`mqtt://${this.platform.config.mqtt.host}:${this.platform.config.mqtt.port}`, mqtt_connection_options);
+    } else {
+      this.mqtt = connect(`mqtt://${this.platform.config.mqtt.host}:${this.platform.config.mqtt.port}`);
+    }
+    this.mqtt.on('connect', () => {
+      this.platform.log.info('Connected to MQTT broker');
+    });
+    this.mqtt.on('error', (error) => {
+      this.platform.log.error('MQTT error:', error);
+    });
+    Object.values(this.topics).forEach((topic) => this.mqtt.subscribe(topic, (error) => { 
+      if (error) {
+        this.platform.log.error('Error subscribing to topic:', error);
+      } else {
+        this.platform.log.info('Subscribed to topic ', topic);
+      }
+    }));
+    this.mqtt.on('message', (topic, message) => {
+      const payload = message.toString();
+      this.platform.log.info(`Received message on topic ${topic}: ${payload}`);
+      switch (topic) {
+      case this.topics.height:
+        this.deskStates.CurrentPosition = this.HeightToPercentage(parseFloat(payload));
+        this.service.updateCharacteristic(this.platform.Characteristic.CurrentPosition, this.deskStates.CurrentPosition);
+        this.platform.log.debug(`Updating characteristic CurrentPosition: ${this.deskStates.CurrentPosition}`);
+        break;
+      }
+    });
+
     // set accessory information
     this.accessory.getService(this.platform.Service.AccessoryInformation)!
       .setCharacteristic(this.platform.Characteristic.Manufacturer, 'Default-Manufacturer')
@@ -31,7 +78,9 @@ export class ExamplePlatformAccessory {
 
     // get the LightBulb service if it exists, otherwise create a new LightBulb service
     // you can create multiple services for each accessory
-
+    this.service = this.accessory.getService(this.platform.Service.WindowCovering) || this.accessory.addService(this.platform.Service.WindowCovering);
+    
+    /*
     if (accessory.context.device.CustomService) {
       // This is only required when using Custom Services and Characteristics not support by HomeKit
       this.service = this.accessory.getService(this.platform.CustomServices[accessory.context.device.CustomService]) ||
@@ -39,23 +88,23 @@ export class ExamplePlatformAccessory {
     } else {
       this.service = this.accessory.getService(this.platform.Service.Lightbulb) || this.accessory.addService(this.platform.Service.Lightbulb);
     }
-
+    */
     // set the service name, this is what is displayed as the default name on the Home app
     // in this example we are using the name we stored in the `accessory.context` in the `discoverDevices` method.
-    this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.exampleDisplayName);
+    this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.name);
 
     // each service must implement at-minimum the "required characteristics" for the given service type
     // see https://developers.homebridge.io/#/service/Lightbulb
 
-    // register handlers for the On/Off Characteristic
-    this.service.getCharacteristic(this.platform.Characteristic.On)
-      .onSet(this.setOn.bind(this)) // SET - bind to the `setOn` method below
-      .onGet(this.getOn.bind(this)); // GET - bind to the `getOn` method below
+    this.service.getCharacteristic(this.platform.Characteristic.CurrentPosition)
+      .onGet(this.getCurrentPosition.bind(this));
 
-    // register handlers for the Brightness Characteristic
-    this.service.getCharacteristic(this.platform.Characteristic.Brightness)
-      .onSet(this.setBrightness.bind(this)); // SET - bind to the `setBrightness` method below
+    this.service.getCharacteristic(this.platform.Characteristic.PositionState)
+      .onGet(this.getPositionState.bind(this));
 
+    this.service.getCharacteristic(this.platform.Characteristic.TargetPosition)
+      .onGet(this.getTargetPosition.bind(this))
+      .onSet(this.setTargetPosition.bind(this));
     /**
      * Creating multiple services of the same type.
      *
@@ -67,13 +116,13 @@ export class ExamplePlatformAccessory {
      * can use the same subtype id.)
      */
 
-    // Example: add two "motion sensor" services to the accessory
+    /* Example: add two "motion sensor" services to the accessory
     const motionSensorOneService = this.accessory.getService('Motion Sensor One Name')
       || this.accessory.addService(this.platform.Service.MotionSensor, 'Motion Sensor One Name', 'YourUniqueIdentifier-1');
 
     const motionSensorTwoService = this.accessory.getService('Motion Sensor Two Name')
       || this.accessory.addService(this.platform.Service.MotionSensor, 'Motion Sensor Two Name', 'YourUniqueIdentifier-2');
-
+    */
     /**
      * Updating characteristics values asynchronously.
      *
@@ -82,7 +131,7 @@ export class ExamplePlatformAccessory {
      * Here we change update the motion sensor trigger states on and off every 10 seconds
      * the `updateCharacteristic` method.
      *
-     */
+     
     let motionDetected = false;
     setInterval(() => {
       // EXAMPLE - inverse the trigger
@@ -95,19 +144,113 @@ export class ExamplePlatformAccessory {
       this.platform.log.debug('Triggering motionSensorOneService:', motionDetected);
       this.platform.log.debug('Triggering motionSensorTwoService:', !motionDetected);
     }, 10000);
+    */
+    this.platform.log.debug('configuring desk: ', this.accessory.context.device);
+  }
+
+  PercentageToHeight(percentage: number) {
+    percentage = percentage > 100 ? 100 : percentage < 0 ? 0 : percentage;
+    const baseHeight = this.GetBaseHeightAndMovementRange().baseHeight;
+    const movementRange = this.GetBaseHeightAndMovementRange().movementRange;
+
+    return Math.round(percentage / 100 * movementRange + baseHeight);
+  }
+
+  HeightToPercentage(height: number) {
+    const baseHeight = this.GetBaseHeightAndMovementRange().baseHeight;
+    const movementRange = this.GetBaseHeightAndMovementRange().movementRange;
+
+    let calculatedPercentage = Math.round((height - baseHeight) / movementRange * 100);
+    calculatedPercentage = calculatedPercentage > 100 ? 100 : calculatedPercentage < 0 ? 0 : calculatedPercentage;
+    return calculatedPercentage;
+  }
+
+  GetBaseHeightAndMovementRange() {
+    let baseHeight = 540;
+    let movementRange = 660;
+
+    if (this.platform.config.baseHeight > 0) {
+      baseHeight = this.platform.config.baseHeight;
+    }
+
+    if (this.platform.config.maxHeight > 0) {
+      movementRange = this.platform.config.maxHeight - this.platform.config.baseHeight;
+    }
+
+    return {
+      baseHeight: baseHeight,
+      movementRange: movementRange,
+    };
+  }
+
+  async moveToPercent(percentage: number) {
+    const newHeight = this.PercentageToHeight(percentage);
+
+    // Move by HTTP command
+    const deskGroupType = this.accessory.context.device.desk_type === DeskGroupType.Group ? '/groups/' : '/desks/';
+
+    const url = this.platform.config.sLinakServerBasePath + deskGroupType + this.accessory.context.device.id + '/height';
+    const basic_auth_string = Buffer.from(this.platform.config.username + ':' + this.platform.config.password).toString('base64');
+    const headers = {
+      'Authorization': 'Basic ' + basic_auth_string,
+    };
+
+    const postJSON = bent('POST', 202);
+    await postJSON(url, newHeight.toString(), headers);
+
+    // Check if move is done by calling a GET endpoint (which is queued by the HTTP server)
+    const deskId = this.accessory.context.device.desk_type === DeskGroupType.Group 
+      ? this.accessory.context.device.desks[0].id 
+      : this.accessory.context.device.id;
+    const getUrl = this.platform.config.sLinakServerBasePath + '/desks/' + deskId + '/height';    
+    const getJSON = bent('GET', 'json');
+    await getJSON(getUrl, '', headers);
+
+    this.deskStates.CurrentPosition = percentage;
+    this.service.getCharacteristic(this.platform.Characteristic.CurrentPosition).updateValue(this.deskStates.CurrentPosition);
+
+    this.service.getCharacteristic(this.platform.Characteristic.PositionState)
+      .updateValue(this.platform.Characteristic.PositionState.STOPPED);
+
+    this.platform.log.debug('Move finished to: ', percentage);
   }
 
   /**
    * Handle "SET" requests from HomeKit
    * These are sent when the user changes the state of an accessory, for example, turning on a Light bulb.
-   */
+   
   async setOn(value: CharacteristicValue) {
     // implement your own code to turn your device on/off
     this.exampleStates.On = value as boolean;
 
     this.platform.log.debug('Set Characteristic On ->', value);
   }
+  */
 
+  /**
+   * Handle requests to set the "Target Position" characteristic
+   */
+  async setTargetPosition(value: CharacteristicValue) {
+    this.platform.log.debug('Triggered SET TargetPosition:', value);
+
+    this.deskStates.TargetPosition = value as number;
+    this.service.getCharacteristic(this.platform.Characteristic.TargetPosition).updateValue(this.deskStates.TargetPosition);
+
+    clearTimeout(this.requestedPosTimer);
+    this.requestedPosTimer = setTimeout(() => {
+      this.platform.log.debug('executing move to: ', value);
+
+      const moveUp = value as number > this.deskStates.TargetPosition;
+      const positionState = moveUp ? this.platform.Characteristic.PositionState.INCREASING
+        : this.platform.Characteristic.PositionState.DECREASING;
+
+      // Tell HomeKit we're on the move.
+      this.service.getCharacteristic(this.platform.Characteristic.PositionState).updateValue(positionState);
+
+      setTimeout(() => this.moveToPercent(value as number), 100);
+
+    }, 500);
+  }
   /**
    * Handle the "GET" requests from HomeKit
    * These are sent when HomeKit wants to know the current state of the accessory, for example, checking if a Light bulb is on.
@@ -122,7 +265,7 @@ export class ExamplePlatformAccessory {
 
    * @example
    * this.service.updateCharacteristic(this.platform.Characteristic.On, true)
-   */
+   
   async getOn(): Promise<CharacteristicValue> {
     // implement your own code to check if the device is on
     const isOn = this.exampleStates.On;
@@ -133,16 +276,61 @@ export class ExamplePlatformAccessory {
     // throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
 
     return isOn;
+  } */
+
+  /**
+   * Handle requests to get the current value of the "Target Position" characteristic
+   */
+  async getTargetPosition(): Promise<CharacteristicValue> {
+    this.platform.log.debug('Triggered GET TargetPosition');
+    this.platform.log.debug(this.deskStates.TargetPosition.toString());
+    
+    return this.deskStates.TargetPosition;
   }
 
   /**
-   * Handle "SET" requests from HomeKit
-   * These are sent when the user changes the state of an accessory, for example, changing the Brightness
+   * Handle requests to get the current value of the "Position State" characteristic
    */
-  async setBrightness(value: CharacteristicValue) {
-    // implement your own code to set the brightness
-    this.exampleStates.Brightness = value as number;
+  async getPositionState(): Promise<CharacteristicValue> {
+    this.platform.log.debug('Triggered GET PositionState');
 
-    this.platform.log.debug('Set Characteristic Brightness -> ', value);
+    // set this to a valid value for PositionState
+    const moveUp = this.deskStates.CurrentPosition > this.deskStates.TargetPosition;
+    return moveUp 
+      ? this.platform.Characteristic.PositionState.INCREASING
+      : this.deskStates.CurrentPosition < this.deskStates.TargetPosition 
+        ? this.platform.Characteristic.PositionState.DECREASING 
+        : this.platform.Characteristic.PositionState.STOPPED;
+  }
+
+  /**
+     * Handle requests to get the current value of the "Current Position" characteristic
+     */
+  async getCurrentPosition(): Promise<CharacteristicValue> {
+    this.platform.log.debug('Triggered GET CurrentPosition');
+
+    // Get height from HTTP server
+    const deskId = this.accessory.context.device.desk_type === DeskGroupType.Group 
+      ? this.accessory.context.device.desks[0].id
+      : this.accessory.context.device.id;
+    const url = this.platform.config.sLinakServerBasePath + '/desks/' + deskId + '/height';    
+    const basic_auth_string = Buffer.from(this.platform.config.username + ':' + this.platform.config.password).toString('base64');
+    const headers = {
+      'Authorization': 'Basic ' + basic_auth_string,
+    };
+
+    const get = bent('GET', 'json');
+    const response = await get(url, '', headers);
+
+    this.deskStates.CurrentPosition = this.HeightToPercentage(response as number);
+    // This the desk can be moved outside of homekit, TargetPosition has to be updated to match:
+    // this.deskStates.TargetPosition = this.deskStates.CurrentPosition;
+    // this.service.updateCharacteristic(this.platform.Characteristic.TargetPosition, this.deskStates.TargetPosition);
+    
+    // if you need to return an error to show the device as "Not Responding" in the Home app:
+    // throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
+    this.platform.log.debug(this.deskStates.CurrentPosition.toString());
+
+    return this.deskStates.CurrentPosition;
   }
 }
